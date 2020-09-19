da36dc304fa10d135888e3f456d8599d83478953
Mikhail Spiridonov <Muxa2793@mail.ru>
2020-09-19 13:25:57 +0300

    исправил замечания

--- bus_stop.py
+++ bus_stop.py
@@ -1,9 +1,8 @@
 import csv
 from collections import Counter
 
-street = []
-
 def main():
+    street = []
     with open('bus_stop.csv', 'r', encoding='cp1251') as f:
 
         reader = csv.DictReader(f, delimiter=';')
@@ -11,8 +10,8 @@ def main():
         for row in reader:
             street.append(row["Street"])
     
-    most_common_street = Counter(street).most_common(1)
-    list_street = most_common_street[0]
+    most_common_street = Counter(street).most_common(2)
+    list_street = most_common_street[1]
     list_street = list(list_street)
 
     print(f'Улица "{list_street[0]}" встречается {list_street[1]} раз')
