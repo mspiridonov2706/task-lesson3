467c5ac4784f74d3cc0363bf0c88cabc357fdcb1
Mikhail Spiridonov <Muxa2793@mail.ru>
2020-09-19 12:20:50 +0300

    first commit

---
+++ bus_stop.py
@@ -0,0 +1,21 @@
+import csv
+from collections import Counter
+
+street = []
+
+def main():
+    with open('bus_stop.csv', 'r', encoding='cp1251') as f:
+
+        reader = csv.DictReader(f, delimiter=';')
+
+        for row in reader:
+            street.append(row["Street"])
+    
+    most_common_street = Counter(street).most_common(1)
+    list_street = most_common_street[0]
+    list_street = list(list_street)
+
+    print(f'Улица "{list_street[0]}" встречается {list_street[1]} раз')
+
+if __name__ == "__main__":
+    main()
